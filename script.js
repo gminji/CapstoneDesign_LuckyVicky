7e0b3f53d7e3c0630e527e5fedb19f467981d39b
bellymon <cmxmah123@gmail.com>
2024-11-11 19:43:17 +0900

    jinseo

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ function showMainScreen() {
 document.getElementById('dineInButton').addEventListener('click', function() {
     document.getElementById('mainScreen').classList.add('hidden');
     document.getElementById('dineInScreen').classList.remove('hidden');
+    // 매장 식사 화면으로 이동
+    window.location.href = 'dineInDetail.html';  // dineInDetail.html 페이지로 이동
 });
 
 // 포장 주문 화면을 보여주는 함수
 document.getElementById('takeOutButton').addEventListener('click', function() {
     document.getElementById('mainScreen').classList.add('hidden');
     document.getElementById('takeOutScreen').classList.remove('hidden');
+    // 포장 주문 화면으로 이동
+    window.location.href = 'takeOutDetail.html';  // takeOutDetail.html 페이지로 이동
 });
 
-// 매장 식사 버튼 클릭 시 새 페이지로 이동
-document.getElementById('dineInButton').addEventListener('click', function() {
-    window.location.href = 'dineInDetail.html';
+// 결제 버튼 클릭 시 새로운 페이지로 이동
+document.getElementById('payButton').addEventListener('click', function() {
+    window.location.href = 'paymentPage.html';  // 결제 페이지로 이동
 });
-
